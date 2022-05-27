40d7b67606516f593a79bef9ef985e207aeff54b
sozoro <46480375+sozoro@users.noreply.github.com>
2022-05-27 19:13:37 +0900

    parsec

---
+++ default.nix
@@ -0,0 +1,19 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default"}:
+let
+  haskellPackages = (if   compiler == "default"
+                     then nixpkgs.pkgs.haskellPackages
+                     else nixpkgs.pkgs.haskell.packages.${compiler}).override {
+    overrides = self: super: {
+    };
+  };
+  developPackage = haskellPackages.developPackage { root = ./.; };
+  hoogle         = haskellPackages.ghcWithHoogle (hs: with hs;
+                     [ ]);
+in
+  developPackage.overrideAttrs (oldAttrs: with nixpkgs; {
+    buildInputs = oldAttrs.buildInputs ++ [
+      pkgs.hlint
+    ];
+    shellHook = ''
+    '';
+  })
