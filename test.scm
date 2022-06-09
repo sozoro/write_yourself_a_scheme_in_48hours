927de4e22fa17f0eb293cce6a8e9013579570158
sozoro <46480375+sozoro@users.noreply.github.com>
2022-06-09 13:47:08 +0900

    test loop

--- test.scm
+++ test.scm
@@ -15,3 +15,18 @@
 (write (length one-ten))
 (write (map (lambda (x) (+ x 1)) one-ten))
 (write (filter (lambda (x) (not (eqv? x 5))) one-ten))
+
+(define test-var 2)
+(define (loop func) (eval func) (loop func))
+(loop '(write (eval (read))))
+; This REPL is broken ↓
+;   (define test-var 1)
+;   1
+;   test-var
+;   1
+;   (define test-var2 2)
+;   2
+;   test-var2
+;   Getting an unbound variable: test-var2
+;
+; maybe, we should use nested environment
